663f536fc6d963f33ce28fa4d8c08ac93c8e2216
redaphid <iam@aaronherres.com>
2019-07-27 04:17:03 -0700

    get that larry shouting at aaron

--- go_pubsub_test.go
+++ go_pubsub_test.go
@@ -1,6 +1,8 @@
 package main_test
 
 import (
+	"fmt"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -8,5 +10,25 @@ import (
 )
 
 var _ = Describe("GoPubsub", func() {
-	Expect(Broadcaster).NotTo(BeNil())
+	Describe("When broadcasting to one person", func() {
+		var (
+			aaron *Broadcaster
+			larry *Listener
+		)
+		BeforeEach(func() {
+			aaron = BirthBroadcaster("aaron")
+			larry = BirthListener("larry")
+			go larry.Listen(aaron)
+		})
+		Describe("When broadcasting 5 times", func() {
+			BeforeEach(func() {
+				for i := 0; i < 5; i++ {
+					aaron.Shout(fmt.Sprintf("hey x%v", i))
+				}
+			})
+			It("Should have Larry hear 5 messages.", func() {
+				Expect(larry.HeardMessages).To(Equal(5))
+			})
+		})
+	})
 })
--- main.go
+++ main.go
@@ -1,55 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
 
 	"github.com/fatih/color"
 )
 
 //Broadcaster is a guy who shouts into his megaphone
-type broadcaster struct {
+type Broadcaster struct {
 	name      string
 	megaphone chan string
 }
 
-func (b *broadcaster) Megaphone() <-chan string {
+//Megaphone returns a stream of messages.
+func (b *Broadcaster) Megaphone() <-chan string {
 	return b.megaphone
 }
 
-//Shout makes the broadcaster shout
-func (b *broadcaster) Shout(msg string) {
+//Shout makes the Broadcaster shout
+func (b *Broadcaster) Shout(msg string) {
+	color.Magenta("%s shouts: %s", b.name, msg)
 	b.megaphone <- msg
 }
 
-//BirthBroadcaster births a new...broadcaster
-func BirthBroadcaster(name string) *broadcaster {
-	color.Red("%s, a broadcaster, announces it's own birth", name)
-	return &broadcaster{
+//BirthBroadcaster births a new...Broadcaster
+func BirthBroadcaster(name string) *Broadcaster {
+	color.Magenta("%s, a Broadcaster, announces it's own birth", name)
+	return &Broadcaster{
 		name:      name,
 		megaphone: make(chan string),
 	}
 }
 
-func BirthListener(name string) *listener {
-	color.Green("%s, a listener, politely enters the room.", name)
-	return &listener{
+//BirthListener gives birth to a listener
+func BirthListener(name string) *Listener {
+	color.Green("%s, a Listener, politely enters the room.", name)
+	return &Listener{
 		name: name,
 	}
 }
 
-type listener struct {
-	name         string
-	messageCount int
+//Listener is a meek but friendly thing that takes what Broadcaster says to heart
+type Listener struct {
+	name          string
+	HeardMessages int
 }
 
-func (l *listener) Listen(b *broadcaster) {
+//Listen listens to the Broadcaster
+func (l *Listener) Listen(b *Broadcaster) {
 	color.Green("%s: Alright, I'm listening", l.name)
 	megaphone := b.Megaphone()
 	for {
 		select {
 		case msg := <-megaphone:
-			l.messageCount++
+			l.HeardMessages++
 			l.talkAboutIt(msg)
 		case <-time.After(1 * time.Second):
 			l.complainAboutBoredom()
@@ -59,29 +64,17 @@ func (l *listener) Listen(b *broadcaster) {
 	panic("what the hell am I doing here? I don't belong here.")
 }
 
-func (l *listener) talkAboutIt(msg string) {
-	color.Blue("%s: msg#%v: %s", l.name, l.messageCount, msg)
+func (l *Listener) talkAboutIt(msg string) {
+	color.Green("%s: msg#%v: %s", l.name, l.HeardMessages, msg)
 	if msg == "bye!" {
 		color.Yellow("%s: I guess I should stop listening.", l.name)
 	}
 }
 
-func (l *listener) complainAboutBoredom() {
-	color.Blue("%s got bored after %v messages", l.name, l.messageCount)
+func (l *Listener) complainAboutBoredom() {
+	color.Blue("%s got bored after %v messages", l.name, l.HeardMessages)
 }
 
-const watchCount = 1
-const shoutTimes = 5
-
 func main() {
-	fmt.Println("Starting up")
-	aaron := BirthBroadcaster("aaron")
-	larry := BirthListener("larry")
-	go larry.Listen(aaron)
-	for i := 0; i < shoutTimes; i++ {
-		aaron.Shout(fmt.Sprintf("hey x%v", i))
-		time.Sleep(100)
-	}
-	aaron.Shout("bye!")
-	time.Sleep(100 * time.Millisecond)
+	log.Printf("Go away. I do nothing now.")
 }
